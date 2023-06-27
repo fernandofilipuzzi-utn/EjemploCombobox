c9ba739b6e2d0eeadf7a8ac5963ce4ecd729b962
fernandofilipuzzi.utn@gmail.com <fernandofilipuzzi.utn@gmail.com>
2023-06-26 22:24:43 -0300

    ejemplo 1

---
+++ EjemploCombobox/EjemploCombobox/FormPrincipal.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace EjemploCombobox
+{
+    partial class FormPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(58, 50);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(283, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(286, 81);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // FormPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(499, 154);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "FormPrincipal";
+            this.Text = "Consulta de reguistros de conducir";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
---
+++ EjemploCombobox/EjemploCombobox/FormPrincipal.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EjemploCombobox
+{
+    public partial class FormPrincipal : Form
+    {
+        //no uso una clase para gestionar y contener
+        //no la necesito para esta situación
+        RegistroConducir[] registros = new RegistroConducir[100];
+        int cant = 0;
+        
+        RegistroConducir selected = null;
+
+        public FormPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            #region inicializo el contenedor
+            registros[cant++] = new RegistroConducir("Romina", "A");
+            registros[cant++] = new RegistroConducir("Sabrina", "B");
+            registros[cant++] = new RegistroConducir("Juan", "B");
+            registros[cant++] = new RegistroConducir("GPT", "C");
+            #endregion
+
+            #region inicializando el combo!
+            for (int n = 0; n < cant; n++)
+                comboBox1.Items.Add(registros[n].Nombre);
+            #endregion
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex > -1)
+            {
+                //notar que el indice del combobox es congruente con el indice del arreglo
+                selected = registros[comboBox1.SelectedIndex];
+
+                #region muestro la info del registro seleccionada
+                numericUpDown1.Value = selected.Puntos;
+                numericUpDown1.Enabled = true;
+                label1.Text = string.Format("{0} ({1})", selected.Nombre, selected.Categoria);
+                #endregion
+            }
+            else
+            {
+                #region muestro la info del registro seleccionada
+                numericUpDown1.Value = 0;
+                numericUpDown1.Enabled = false;
+                label1.Text = "";
+                #endregion
+            }
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            if(selected!=null)
+                selected.Puntos= Convert.ToInt32(numericUpDown1.Value);
+        }
+    }
+}
